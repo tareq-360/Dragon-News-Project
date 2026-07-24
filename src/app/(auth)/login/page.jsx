1e3b14719796bb5cdc165fbe019b97c0e412dba7
tareq-360 <mdtareqhossain2002@gmail.com>
2026-07-24 15:39:32 +0600

    login form done

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,33 @@
-
+'use client'
+import Link from "next/link";
+import { useForm } from "react-hook-form"
 const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
     return (
-        <div>
-            Login
+        <div className=" container flex justify-center items-center mx-auto bg-slate-100 p-20 rounded-md">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 shadow-md">
+                    <h2 className=" text-center font-bold text-xl p-5">Login your account</h2>
+
+
+                    <label className="label">Email</label>
+                    <input {...register("email", { required: "Email field is empty" })} type="email" className="input" placeholder="Email" />
+                    {errors.email && <span className=" text-red-500 font-semibold">{errors.email.message}</span>}
+                    <label className="label">Password</label>
+                    <input {...register("password", { required: "Password field is empty" })} type="password" className="input" placeholder="Password" />
+                    {errors.password && <span className=" text-red-500 font-semibold">{errors.password.message}</span>}
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+                    <p>Dont’t Have An Account ? <Link href="/register"><small className=" text-red-500 font-bold"> Register</small></Link></p>
+                </fieldset>
+            </form>
         </div>
     );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,8 +1,31 @@
-
+import Link from "next/link";
 const RegisterPage = () => {
     return (
-        <div>
-            Register
+        <div className=" container flex justify-center items-center mx-auto bg-slate-100 rounded-md  p-10">
+            <form>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 shadow-md">
+                    <h2 className=" text-center font-bold text-xl p-5">Register your account</h2>
+
+
+                    <label className="label">Name</label>
+                    <input type="text" className="input" placeholder="Your Name" />
+                    <label className="label">Photo URL</label>
+                    <input type="text" className="input" placeholder="Your Photo URL" />
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" />
+
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                    <label className="label">
+                        <input type="checkbox" required className="checkbox" />
+                        Accept Trams & Conditions
+                    </label>
+                    <button className="btn btn-neutral mt-4">Register</button>
+
+                    <p>Do You Have An Account ? <Link href="/login"><small className=" text-red-500 font-bold"> Login</small></Link></p>
+
+                </fieldset>
+            </form>
         </div>
     );
 };
