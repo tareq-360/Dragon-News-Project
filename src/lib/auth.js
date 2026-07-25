e3107cee6e56c9ce9a708184e722a55a558c3540
tareq-360 <mdtareqhossain2002@gmail.com>
2026-07-25 14:39:40 +0600

    deployment problem try to fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.AUTH_URI);
+const client = new MongoClient(process.env.AUTH_URI || "http://localhost:3000");
 const db = client.db();
 
 export const auth = betterAuth({
