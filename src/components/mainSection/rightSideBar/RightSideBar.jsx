95b8d8c4f275afc8172e159864415e822a8e2389
tareq-360 <mdtareqhossain2002@gmail.com>
2026-07-27 12:17:04 +0600

    google and githun Signin operation done

--- src/components/mainSection/rightSideBar/RightSideBar.jsx
+++ src/components/mainSection/rightSideBar/RightSideBar.jsx
@@ -1,15 +1,30 @@
-import { FaGoogle,FaGithub } from "react-icons/fa";
+'use client'
+import { authClient } from "@/lib/auth-client";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
+const handleGoogleSignin = async () => {
+    const data = await authClient.signIn.social({
+        provider: "google",
+    });
+    console.log("User Data ", data);
+}
+const handleGithubSignin = async () => {
+    const data = await authClient.signIn.social({
+        provider: "github"
+    })
+    console.log("User Data ", data);
+
+}
 const RightSideBar = () => {
     return (
         <div>
             <h2 className=" font-bold text-xl ">Login With</h2>
             <div className=" flex flex-col gap-2">
-                <button className=" btn btn-outline text-blue-700">
+                <button onClick={handleGoogleSignin} className=" btn btn-outline text-blue-700">
                     <FaGoogle></FaGoogle>
                     Login with google
                 </button>
-                <button className=" btn btn-outline">
+                <button onClick={handleGithubSignin} className=" btn btn-outline">
                     <FaGithub></FaGithub>
                     Login with github
                 </button>
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,11 +3,21 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.AUTH_URI || "http://localhost:3000");
-const db = client.db();
+const db = client.db("dragon-newsDB");
 
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID , 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET , 
+        }, 
     },
     database: mongodbAdapter(db, {
         // Optional: if you don't provide a client, database transactions won't be enabled.
