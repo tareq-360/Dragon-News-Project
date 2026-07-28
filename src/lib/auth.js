9aff6e3e178abdf12b7877d9112d5a68a1459f75
tareq-360 <mdtareqhossain2002@gmail.com>
2026-07-28 11:46:53 +0600

    all done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.AUTH_URI || "http://localhost:3000");
+const client = new MongoClient(process.env.AUTH_URI);
 const db = client.db("dragon-newsDB");
 
 export const auth = betterAuth({
